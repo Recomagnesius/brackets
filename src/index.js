fd26ca294953c1deba8ffabd015ea4f23d27af61
Recomagnesius <buzhor.danyl@gmail.com>
2021-03-15 22:27:24 +0200

    feat: 1.0

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    if(str.length%2>0) return false;
+  
+    let result = '';
+    let openBracket = '';
+    let closeBracket = '';
+    for(let i = 0; i<bracketsConfig.length; ++i){
+      openBracket+=String(bracketsConfig[i][0]);
+      closeBracket+=String(bracketsConfig[i][1]);
+    }
+  
+    for(let i = 0; i!=str.length; ++i){
+      if(result.length==0) {
+        result=result + str[i];
+        continue;
+      }
+  
+      if(openBracket.includes(str[i]) && !closeBracket.includes(str[i])) result+=str[i];
+      else if(openBracket.includes(str[i]) && closeBracket.includes(str[i])){
+        if(result[result.length-1]==str[i]) result = result.slice(0, -1);
+        else result=result + str[i];
+      }else{
+        let indic = 0;
+        while(str[i]!=closeBracket[indic] && indic<closeBracket.length) ++indic;
+        if(result[result.length-1]==openBracket[indic]) result = result.slice(0, -1);
+        else {result='wrong';
+        break;
+      }
+      }
+  
+    }
+  
+    return (result.length==0)
+  }
+  
